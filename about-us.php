62150b74142a473b6452f931a364fbf08d942881
Adil Shaikh <anshaik@hotmail.com>
2015-09-05 13:25:32 -0500

    img name change

--- about-us.php
+++ about-us.php
@@ -10,31 +10,31 @@
 
 						<div class="col-md-5">
 							<div class="features-2-image  " >
-								<img src="resources/images/About Us/Team.jpg" alt="">
+								<img src="resources/images/about/hw-logo.jpg" alt="">
 							</div> <!-- end features-image -->
 						</div> <!-- end col-md-5 -->
 
 						<div class="col-md-6 marg3">
 							<div class="title " >
 								<h2>What is Hardware Library?</h2>
-								<p>Hardware Library started because of a dream. A dream to be able to experience the wonders 
+								<p>Hardware Library started because of a dream. A dream to be able to experience the wonders
 								that is the technology of the 21st century. In a world where our tech is not only
-								being improved upon but revolutionized, we consistently watch the impossible become a reality. 
+								being improved upon but revolutionized, we consistently watch the impossible become a reality.
 								But there is one barrier that is difficult to overcome and prevents
 								developers, early-adopters, even the average consumer from enjoying these advancements.
 								</p>
 								<p>Money.</p>
-								<p>I had this problem. As a programming hobbiest, I was always keeping up with the next big 
+								<p>I had this problem. As a programming hobbiest, I was always keeping up with the next big
 								thing. In my time, it was the Oculus Rift. Virtual reality was taking off
 								and I wanted to get in on it. But I couldn't afford one and so I was relegated to the sidelines.
 								 I searched endlessly for a cheaper alternative but none could be found unless I
 								joined an organizaton or took classes at a university. Nothing helped me experience what I wanted
-								 without breaking the bank. So I created Hardware Library. By carrying the brunt of the costs, we 
-								 allow people like you to experience the latest cutting-edge technology as soon as it's available, at 
+								 without breaking the bank. So I created Hardware Library. By carrying the brunt of the costs, we
+								 allow people like you to experience the latest cutting-edge technology as soon as it's available, at
 								 a fraction of the cost.</p>
-								<p>With adhoc rentals, people can try things out before they buy them, rent hardware for events, 
+								<p>With adhoc rentals, people can try things out before they buy them, rent hardware for events,
 								or even just satisfy that itch to try something cool. </p>
-								<p>For the developer or early adopter who doesn't want to pay a fluctuating price for every item, 
+								<p>For the developer or early adopter who doesn't want to pay a fluctuating price for every item,
 								we have our memberships. By paying a small amount per month, they have access to
 								all of the hardware and the flexibility to renew their rentals automatically!</p>
 								<p>Now there is an alternative. </p>
@@ -50,10 +50,7 @@
 			</div>
 		</section>
 		<!-- End Members-section -->
-        
+
 <?php
 	require('includes/site_footer.php');
 ?>
-
-		
-        
